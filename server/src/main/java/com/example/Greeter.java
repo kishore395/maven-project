cb4d6c46ed914c00d36013f67fce0be11dff4ecc
Moorthy Kishore <moorthyk@Convergence.com>
2018-05-05 19:48:10 +0530

    fix checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,9 @@ public class Greeter {
   }
   
  /**
-   * This is a constructor.
-   */
+   * @param someone the name of person
+   * @return greeting string
+   */   
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
